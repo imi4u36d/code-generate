ea845e6837dc998374a56a18de63ad051bf39f13
imi4u36d <wxwzmail@163.com>
2025-02-28 15:00:30 +0800

    优化文件生成逻辑，支持文件生成在分层结构下

--- src/main/java/com/imi4u36d/model/FileType.java
+++ src/main/java/com/imi4u36d/model/FileType.java
@@ -5,14 +5,35 @@ package com.imi4u36d.model;
  */
 
 public enum FileType {
-    CONTROLLER,
-    ENTITY,
-    SERVICE,
-    IMPL,
-    MAPPER,
-    XML,
-    DTO,
-    BASERESDTO,
-    RES,
-//    JSON;
+    CONTROLLER("Controller.java", "controller.ftl", "controller"),
+    ENTITY(".java", "entity.ftl", "entity"),
+    SERVICE("Service.java", "service.ftl", "service"),
+    IMPL("ServiceImpl.java", "impl.ftl", "service"),
+    MAPPER("Mapper.java", "mapper.ftl", "mapper"),
+    XML("Mapper.xml", "xml.ftl", "mapper"),
+    DTO("Dto.java", "dto.ftl", "dto"),
+    BASERESDTO("BaseResponseDto.java", "baseResponseDto.ftl", "dto"),
+    RES("Result.java", "result.ftl", "res");
+
+    private final String extension;
+    private final String ftlName;
+    private final String packageName;
+
+    FileType(String extension, String ftlName, String packageName) {
+        this.extension = extension;
+        this.ftlName = ftlName;
+        this.packageName = packageName;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getFtlName() {
+        return ftlName;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
 }
